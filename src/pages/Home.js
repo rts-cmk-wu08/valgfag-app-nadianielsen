bc48d2b04b171151eb95e090ddb42f7edeb9bfce
nadianielsen <U2404561@rts-365.dk>
2023-08-28 12:31:34 +0200

    if's and else's about if there is no content on page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,12 +26,16 @@ const Home = () => {
             onChange={event => setLocation(event.target.value)} 
             onKeyUpCapture={searchLocation} 
             placeholder="  Enter Location"/>
+            {data.main ? 
             <article className="flex flex-col gap-y-2 justify-center items-center place-self-center bg-neutral-300/30 w-60 py-4 rounded-2xl shadow-md">
                 <h2 className="text-3xl">{data.name}</h2>
                 {data.main ? <h1 className="text-lg">{data.main.temp} C&deg;</h1> : null}
                 {data.weather ? <p>{data.weather[0].main}</p> : null}
                 {/* <p className="relative -rotate-90 -right-20">clouds</p> */}
             </article>
+            : null
+            }
+            {data.main ?
             <article className="grid grid-flow-col gap-x-6 bg-neutral-300/30 shadow-md rounded-2xl w-72 place-self-center py-4">
                 <section className="place-self-center">
                     <h2>Feels like:</h2>
@@ -46,6 +50,8 @@ const Home = () => {
                     {data.main ? <p>{data.main.temp_max} C&deg;</p> : null }
                 </section>
             </article>
+            : null
+            }
         </article>
      );
 }
