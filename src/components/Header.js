d0cbe957bdc38f4b4e31527ae418e8d941277632
nadianielsen <U2404561@rts-365.dk>
2023-08-28 11:08:17 +0200

    removed a warning

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-import Navigation from "./Navigation";
 
 
 const Header = () => {
     return ( 
         <header className="flex justify-center py-2">
            <h1 className="capitalize font-semibold text-xl">Weather app</h1>
-            {/* <Navigation /> */}
         </header>
      );
 }
