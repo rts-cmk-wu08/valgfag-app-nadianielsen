29e9e3e32888bd030565322b21a49d4693ee6004
nadianielsen <U2404561@rts-365.dk>
2023-08-31 09:55:41 +0200

    styling geolocation page

--- src/pages/GeoLocation.js
+++ src/pages/GeoLocation.js
@@ -23,7 +23,7 @@ const GeoLocation = () => {
     useEffect(() => {
         //laver en if statement hvor den tjekker om latitude og longitude er der
         if(latitude && longitude) {
-            let finalAPIEndPoint = `${APIEndpoint}lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${APIKey}`
+            let finalAPIEndPoint = `${APIEndpoint}lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${APIKey}&units=metric`
             // console.log(finalAPIEndPoint)
             axios.get(finalAPIEndPoint)
                 .then((response) => {
@@ -36,13 +36,22 @@ const GeoLocation = () => {
 
     //resData er her, fordi man ved ikke hvornår dataen kommer fra useEffecten, hvis resdata ikke står der i returnet
     return !resData ? (<p>Finding location...</p>) : (
-        <article className="grid gap-y-2 my-2">
-            <h1 className="text-xl p-2 flex gap-x-2 font-semibold">Your location <FaArrowRight className="my-1.5"/></h1>
-            <article className="w-52 py-6 bg-neutral-300/50 border-2 rounded-2xl place-self-center text-center">
-                <h2 className="text-xl font-semibold">{resData.name}</h2>
-                <p className="text-lg">{resData.sys.country}</p>
-            </article>
+    <article className="grid gap-y-6 my-4">
+        {/* <article className="flex flex-col gap-y-2 justify-center items-center place-self-center bg-neutral-300/30 w-60 py-4 rounded-2xl shadow-md">
+            <h2 className="text-xl font-bold">{resData.name}</h2>
+            <p>{resData.sys.country}</p>
+            <p className="text-lg font-semibold">{resData.main.temp} C&deg;</p>
+            <p>{resData.weather[0].main}</p>
+        </article> */}
+            <h2 className="text-xl font-bold place-self-center">{resData.name}</h2>
+        <article className="w-60 h-60 bg-neutral-300/50 border-2 rounded-full place-self-center flex flex-col justify-center items-center gap-y-3 shadow-md">
+            <p>{resData.weather[0].main}</p>
+            <p className="text-3xl font-semibold">{resData.main.temp} C&deg;</p>
+            <img className="w-16 h-16" src={`https://openweathermap.org/img/wn/${resData.weather[0].icon}@2x.png`} alt="weatherIcon" />
         </article>
+        <article className="w-[18rem] h-[17rem] bg-white/40 place-self-center rounded-2xl border-2">
+        </article>
+    </article>
     );
 }
 
